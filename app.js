537b148109ff006cf9052492aa218cf96e8f2e8a
ihab1990 <ihab.abumulaweh@gmail.com>
2020-12-24 19:33:19 +0200

    Final

--- app.js
+++ app.js
@@ -15,18 +15,17 @@ else if (answer === 'no') {
     (alert('go to the doctor first'))
 }
 
-
 alert('you can reach us on ' + '0775299163');
 
 var ans = prompt('verfiy you are not a robot enter a number from 1 to 5')
 
-if (ans >= 1 && ans <= 5) {
+var j = 0;
 
-   
-
-        (document.write('<img src="https://images.theconversation.com/files/350865/original/file-20200803-24-50u91u.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=675.0&fit=crop">'))
-
+while (ans > j) {
+    (document.write('<img src="https://images.theconversation.com/files/350865/original/file-20200803-24-50u91u.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=675.0&fit=crop">'))
+    j++;
 }
+
 
 
 
