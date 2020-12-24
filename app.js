64ddb9a558843049b6ee1acbb6db598fd735baaf
ihab1990 <ihab.abumulaweh@gmail.com>
2020-12-24 18:51:39 +0200

    if

--- app.js
+++ app.js
@@ -1,19 +1,32 @@
 alert('This is a medical suuplies shop');
 
-
-
-var answer = prompt('Do you have a medical report, Enter yes/No');
+var answer = prompt('Do you have a medical report, Enter  yes/ No');
 
 document.write('<p>' + answer + '</p>');
 
-if (answer == 'yes') {
-    alert('welcome to the shop');
+while (answer !== 'yes' && answer !== 'no') {
+    answer = prompt('Do you have a medical report, Enter yes/No');
+}
 
-} else {
-    alert('go to the doctor first');
-
+if (answer === 'yes') {
+    (alert('welcome to the shop'))
+}
+else if (answer === 'no') {
+    (alert('go to the doctor first'))
 }
 
 
 alert('you can reach us on ' + '0775299163');
+
+var ans = prompt('verfiy you are not a robot enter a number from 1 to 5')
+
+if (ans >= 1 && ans <= 5) {
+
+   
+
+        (document.write('<img src="https://images.theconversation.com/files/350865/original/file-20200803-24-50u91u.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=675.0&fit=crop">'))
+
+}
+
+
 
